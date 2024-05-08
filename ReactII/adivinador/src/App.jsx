1d0086aa26179fdd6350d2b302f01fe3d88399a9
Estefany Clara <estefany.clara@correo.ucu.edu.uy>
2024-05-08 18:00:42 -0300

    Adivinador

---
+++ ReactII/adivinador/src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import { Button } from "rsuite"
+import './App.css'
+import MyButton from './components/MyButton'
+
+
+export default function App() {
+
+  const [numberMin, setNumberMin]= useState(0);
+  const [numberMax, setNumberMax]= useState(10);
+  const [randomNumber, setRandomNumber] = useState(Math.floor(Math.random() * numberMax-numberMin + 1) + numberMin);
+  
+
+  function generateRandomNumber () {
+    setNumberMax(10);
+    setNumberMin(0);
+    setRandomNumber(Math.floor(Math.random() * (numberMax - numberMin + 1)) + numberMin);
+  }
+  const getRandomMenor = () =>{
+    setNumberMax(randomNumber-1);
+    setRandomNumber(Math.floor(Math.random() * (randomNumber -numberMin + 1))+ numberMin);
+  }
+  const getRandomMayor = () =>{
+    setNumberMin(randomNumber+1);
+    setRandomNumber(Math.floor(Math.random() * (numberMax - randomNumber + 1)) + randomNumber);
+  }
+  const lables = ['MAYOR', 'MENOR', 'IGUAL'];
+
+  return (
+    <div className='App'>
+      
+      <h1>{randomNumber}</h1>
+      <MyButton label='Generar número' onClickHandler={generateRandomNumber} variant='blue'/>
+      <MyButton label={lables[0]} onClickHandler={getRandomMayor}  variant='blue'/>
+      <MyButton label={lables[1]} onClickHandler={getRandomMenor}  variant='red'/>
+      <MyButton label={lables[2]} onClickHandler={alert} variant='green'/>
+    </div>
+  )
+}
---
+++ ReactII/adivinador/src/components/MyButton/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import classes from "./index.module.css";
+
+
+const MyButton = ({label, onClickHandler, variant}) => {
+    const buttonClick= () => {
+        onClickHandler();
+    };
+    
+    return(
+    <button className={classes[variant]} onClick={()=>{onClickHandler()}}>
+    {label} 
+    </button>
+)
+};
+export default MyButton;
+  
